f5bc3310fb6fd1791df71ecd6662a308273a1f2a
XiaoKe89 <86465316+XiaoKe89@users.noreply.github.com>
2025-04-16 01:52:20 -0600

    Delete docker directory

--- docker/scripts/create_netcat.sh
+++
@@ -1,1 +0,0 @@
-docker compose exec matrix  bash -c "apt-get update && apt-get install netcat -y" 
--- docker/scripts/do-dump.sh
+++
@@ -1,3 +0,0 @@
-docker-compose exec  postgres bash   ./dump_db.sh
-docker-compose cp postgres:/backup/ ./
-ls -l ./backup
